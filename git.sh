67bf55a2bd4c80604e64da8139e7198b4457334a
mmdreza21 <mrezaj11@live.com>
2025-04-13 21:02:26 +0330

    sh file updated

--- git.sh
+++ git.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+
+
+if [ -z "$1" ]; then
+  echo "Error: Please provide a commit message"
+  echo "Usage: ./git.sh \"your commit message\""
+  exit 1
+fi
+
 git pull
 echo "__________________________________"
 git add .
 echo "__________________________________"
-git commit -m $
+git commit -m "$1"
 echo "__________________________________"
 git push
-
